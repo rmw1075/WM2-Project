3d88327a0ac35dc4848539739d2678ff8dbbb5aa
rmw1075 <rmw1075@g.rit.edu>
2019-11-13 15:24:34 -0500

    quiz acting up, see comments in grading.php

--- assets/php/grading.php
+++ assets/php/grading.php
@@ -14,24 +14,23 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == false) {
 ?>
 
 <?php
-function test_input($data) {
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
+for ($a = 0; $a < count($_SESSION["qorder"]); $a++){
+    echo $_SESSION["qorder"][$a];
 }
 
 $questionOrder = $_SESSION["qorder"];
 $optionOrder = $_SESSION["opOrder"];
+echo $_SESSION["userID"];
 $answered = array();
-for ($a = 1; $a <= count($questionOrder); $a++){
-    $answer = test_input($_POST["option$a"]);
+for ($a = 0; $a < count($questionOrder); $a++){
+    $answer = $_POST["option$a"];
     array_push($answered, $answer);
 }
-$corectTotal = "";
-
+$correctTotal = 0;
+echo "Here";
 for ($a = 0; $a < count($questionOrder); $a++) {
-    $stmt = "SELECT qnum, correct FROM quiz WHERE qnum='$questionOrder[$a]'";
+    $pointer = $questionOrder[$a];
+    $stmt = "SELECT qnum, correct FROM quiz WHERE qnum='$pointer'";
     $result = mysqli_query($mysqli, $stmt);
     if (mysqli_num_rows($result) > 0) {
         while ($row = $result->fetch_assoc()) {
@@ -45,7 +44,30 @@ for ($a = 0; $a < count($questionOrder); $a++) {
 
 $total = eval('return '.$correctTotal."/".count($questionOrder).';');
 $date = date('Y-m-d H:i:s');
-$stmt = $mysqli->prepare("INSERT INTO rmw1075.results(userID, quizDate, qOrder, answerOrder, chosen, score) VALUES (?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("ssssss", $userID, $name, $hashedpass, $email, $role);
-$stmt->execute();
+echo "Here 2";
+$optionsImploded = array();
+foreach ($optionOrder as $op) {
+    $optionString = implode("-", $op);
+    array_push($optionsImploded, $optionString);
+}
+echo "Here3";
+
+// An error is being thrown here : binding_error
+try {
+    $stmt = $mysqli->prepare("INSERT INTO rmw1075.results(userID, quizDate, qOrder, answerOrder, chosen, score) VALUES (:userID, :quizDate, :qOrder, :answerOrder, :chosen, :score)");
+    $naame = $_SESSION["userID"];
+    $stmt->bind_param(":userID", $naame);
+    echo "TEEEEEST";
+    $stmt->bind_param(":quizDate", $date);
+    $stmt->bind_param(":qOrder", implode(',', $questionOrder));
+    $stmt->bind_param(":answerOrder", implode(',', $optionsImploded));
+    $stmt->bind_param(":qOrder", implode(',', $answered));
+    $stmt->bind_param(":score", $total);
+    $stmt->execute();
+    $stmt->close();
+    echo "Here4";  
+} catch(Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+
 ?>
--- assets/php/quiz.php
+++ assets/php/quiz.php
@@ -16,23 +16,31 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == false) {
 $stmt = "SELECT * FROM quiz";
 $results = mysqli_query($mysqli, $stmt);
 $questions = array();
+
 if (mysqli_num_rows($results) > 0) {
     while ($row = $results->fetch_assoc()) {
         $q = array($row["qnum"], $row["question"], $row["correct"], $row["option2"], $row["option3"], $row["option4"]);
         array_push($questions, $q);
     }
 }
+
+$qOrder = array();
 shuffle($questions);
+foreach ($questions as $a) {
+  array_push($qOrder, $a[0]);  
+}
+
 echo "<form name=\"quizForm\" method=" . "\"POST\"" ." action=" . "\"grading.php\"" . "><br />";
 $optionsOrder = array();
 for($a = 0; $a < count($questions); $a++) {
-    $question = $questions[$a][1];
-    $options = array($questions[$a][2],$questions[$a][3],$questions[$a][4],$questions[$a][5]);
+    $question = $questions[$a];
+    $questionName = $question[1];
+    $options = array($question[2],$question[3],$question[4],$question[5]);
     shuffle($options);
     array_push($optionsOrder, $options);
     $num = eval('return '.$a."+".'1'.';');
     echo "<fieldset>";
-    echo "<legend>" . "$num:" . " $question" . "</legend><br />";
+    echo "<legend>" . "$num:" . " $questionName" . "</legend><br />";
     for ($b = 0; $b < 4; $b++) {
         echo "<input type=". "\"radio\"" . "name=" . "\"option$a\"" . " value=" . "\"$options[$b]\"" . ">";
         echo "$options[$b]";
@@ -44,7 +52,7 @@ for($a = 0; $a < count($questions); $a++) {
 echo "<input type=\"submit\" name=\"submit\" value=\"Submit\" />";
 echo "</form>";
 
-$_SESSION["qorder"] = $questions;
+$_SESSION["qorder"] = $qOrder;
 $_SESSION["opOrder"] = $optionsOrder;
 
 ?>
