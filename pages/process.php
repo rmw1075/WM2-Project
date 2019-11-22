3d4a509246602096fb67d864e092e63da77e3990
Kyrren Love <47565721+kyrrenl@users.noreply.github.com>
2019-11-22 11:59:57 -0500

    Add files via upload
    Adding updated survey and process.php form to send quiz questions to the database;

--- pages/process.php
+++ pages/process.php
@@ -1,8 +1,7 @@
 <?php
-//$message = " Data Sent";
-//$message .= " October 1, 2019\n";
-//echo "Today is sample of PHP, see ---> $message";
-
+$path = './';
+$page = 'Ex08 Comment Form';
+require $path.'/../../../dbConnect.inc';
 function test_input($data){
 	$data = trim($data);
 	$data = stripslashes($data);
@@ -10,76 +9,83 @@ function test_input($data){
 	return $data;
 }
 
-/*if (isset($_POST['name']) ) {
-	$name = test_input($name['name']);
-}
-else {
-	$name = "No visitor name supplied";
-}*/
-$name =       $_POST['name'];
-//$groupNum =   $_POST['groupNum'];
-//$visitDate =  $_POST['visitDate'];
-$topic =      $_POST['favTopic'];
-$rate =       $_POST['rate'];
-//$dest_email = "kcl4926@g.rit.edu";
-//$dest_email = "RITISTprofessor@gmail.com";
+//$topic =      $_POST['favTopic'];
+//$quizrate =       $_POST['quizrate'];
+//$rate =       $_POST['rate'];
 
-$email_subject = "LearnPy Survey";
+//if (isset($_POST['favTopic']) ) {
+//	$topic = test_input($topic['favTopic']);
+//}
+//else {
+//	$topic = "No favTopic supplied";
+//}
+//
+//if (isset($_POST['quizrate']) ) {
+//	$quizrate = test_input($quizrate['quizrate']);
+//}
+//else {
+//	$quizrate = 20;
+//}
+//
+//
+//if (isset($_POST['rate']) ) {
+//	$rate = test_input($rate['rate']);
+//}
+//else {
+//	$rate = 20;
+//}
 
-/*if ($name == ""){
-	$extra = " "
-} 
-else{
-	$email_body = "Visitor name: $name \n";
-}*/
+if ($mysqli) {
+	  if (isset($_POST['favTopic']) && isset($_POST['quizrate'])  && isset($_POST['rate'])   ) {
+	  if( $_POST['favTopic']!='' && $_POST['quizrate']!='' && $_POST['rate']!='' ){
+		$stmt=$mysqli->prepare("INSERT INTO surveycomments (question1, question2, question3) VALUES(?, ?, ?)");
+        $favTopic =      $_POST['favTopic'];
+        $quizrate =       $_POST['quizrate'];
+        $rate =       $_POST['rate'];
+        //echo "Favorite Topic: $favTopic";
+        //echo "";
+        //echo "Quizrate: $quizrate ";
+        //echo "";
+        //echo "rate: $rate ";
+		$stmt->bind_param("sii", $favTopic, $quizrate, $rate);
+		$stmt->execute();
+		$stmt->close();
+	  }//end of if to make sure data is sent using $_GET
+      }//end of isset
+	  //get contents of table and send back...
+      $sql = 'select question1, question2, question3 from surveycomments';
+	  $res=$mysqli->query($sql);
+	  if($res){
+		while($rowHolder = mysqli_fetch_array($res,MYSQLI_ASSOC)){
+			$records[] = $rowHolder;
+		}
+	  }
+	}
 
 
-/*if ($groupNum == ""){
-	$extra = " "
-} 
-else{
-	$email_body .= "Group size: $groupNum \n";
-}*/
 
 
-/*if ($visitDate == ""){
-	$extra = " "
-}
-else{
-	$email_body .= "Date visited: $visitDate \n";
-}*/
 
-/*if ($place == ""){
-	$extra = " "
-}
-else{
-	$email_body .= "Favorite place: $place \n";
-}*/
-
-/*if ($rate == ""){
-	$extra = " "
-}
-else{
-	$email_body .= "Rating --> $rate \n";
-}*/
-
-$email_body = "Visitor name: $name \n";
+//$email_body = "Visitor name: $name \n";
 //$email_body .= "Group size: $groupNum \n";
 //$email_body .= "Date visited: $visitDate \n";
-$email_body .= "Favorite topic: $topic \n";
+//$email_body .= "Favorite topic: $topic \n";
 //$email_body .= "Rating --> $rate \n";
-mail($dest_email, $email_subject, $email_body);
-echo "Email data sent";
+//mail($dest_email, $email_subject, $email_body);
+//echo "Email data sent";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+<?php
+    //include $path '/../inc/header.php';
+?>
 <head>
 </head>
 <body>
 	<figure>
 	<img src="images/thankyou2.png" alt="Thank you" title="Thank you for your feed back"> 
 	</figure>
-Thank you for submitting your feedback on our survey. Your results will be emailed for us to look at.
+Thank you for submitting your feedback on our survey. Your results will be added into our database for us to look at.
 </body>
 </html>
