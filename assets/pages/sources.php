88131133b3a3a715cffcf6ed618bc99ea1ff48d8
sagza12 <56004160+sagza12@users.noreply.github.com>
2019-11-30 00:55:17 -0500

    resources modular page

---
+++ assets/pages/sources.php
@@ -0,0 +1,32 @@
+<?php
+		session_start();
+        $path = './';
+        $page = 'Resources';
+        include $path . '../inc/header2.php';
+        ?>
+    
+
+<?php
+            require $path.'../../dbConnect.inc';            
+		$sql = "SELECT content FROM modularSite where page='$page'";
+		$result = $mysqli->query($sql);
+
+		if($result->num_rows > 0){
+			
+			while ($row = $result->FETCH_ASSOC()) {
+				echo $row['content'];
+			}
+		}else{
+			echo "0 results, did something wrong!";
+		}
+
+    ?>
+
+<?php
+         
+         include $path . '../inc/footer.php';
+    ?>
+<script>
+document.getElementById("sources").style.color = "orange";
+
+</script>
